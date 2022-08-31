cb8af92726fe3e704cebdffb7955eea8b8ae5258
Myada92 <109886095+Myada92@users.noreply.github.com>
2022-08-31 01:33:26 -0400

    message

---
+++ src/main/java/Test2.java
@@ -0,0 +1,13 @@
+
+public class Test2 {
+	public static void main(String[] args) {
+		printme();
+		printme();
+	}
+public static void printme() {
+	System.out.println("git test1");
+}
+public static void printme2() {
+	System.out.println("git test2");
+}
+}
